f3df7c1fbab12e486575632554d036dfc4b16b08
Shivratna <60550869+Shivratna@users.noreply.github.com>
2020-02-23 11:25:50 +0530

    Add files via upload

---
+++ Ball Track/ARDLED/ARDLED.ino
@@ -0,0 +1,24 @@
+const int led=13;
+int value;
+
+void setup() 
+   { 
+      Serial.begin(9600); 
+      pinMode(LED_BUILTIN, OUTPUT);
+      digitalWrite (LED_BUILTIN, LOW);
+   }
+ 
+void loop() 
+   {
+     while (Serial.available())
+        {
+           value = Serial.read();
+        }
+     
+     if (value == '1')
+        digitalWrite (LED_BUILTIN, HIGH);
+      
+     
+     else if (value == '0')
+        digitalWrite (LED_BUILTIN, LOW);
+   }
